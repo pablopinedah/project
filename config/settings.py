09116919897e808abfd75894e26b9c0d504ff36f
Pablo Pineda H <pablopinedah@gmail.com>
2023-06-15 21:39:57 -0500

    Presentacion home

--- config/settings.py
+++ config/settings.py
@@ -136,3 +136,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#! Login
+# Necesario para el login
+
+
+LOGIN_URL = "home:index"
+#LOGIN_REDIRECT_URL = reverse_lazy("home:index")
